8d79e32b6e7da28377eb7448f38ef953eebfe7ce
Alex <alex@Miracle.com>
2012-12-26 18:50:22 -0800

    Ignore Test

---
+++ src/main/java/JunitTest3.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class JunitTest3 {
+
+	@Ignore("Not Ready to Run")  
+	@Test
+	public void divisionWithException() {  
+	  System.out.println("Method is not ready yet");
+	}  
+
+}
