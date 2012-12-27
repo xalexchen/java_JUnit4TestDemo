1fc45846dc3d1e5a0c07f12efa305938a46bd935
Alex <alex@Miracle.com>
2012-12-26 18:45:09 -0800

    initial commit

---
+++ src/test/java/JunitTest1.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JunitTest1 {
+
+	private Collection collection;
+
+	@BeforeClass
+	public static void oneTimeSetUp() {
+		// one-time initialization code
+		System.out.println("@BeforeClass - oneTimeSetUp");
+	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+		// one-time cleanup code
+		System.out.println("@AfterClass - oneTimeTearDown");
+	}
+
+	@Before
+	public void setUp() {
+		collection = new ArrayList();
+		System.out.println("@Before - setUp");
+	}
+
+	@After
+	public void tearDown() {
+		collection.clear();
+		System.out.println("@After - tearDown");
+	}
+
+	@Test
+	public void testEmptyCollection() {
+		assertTrue(collection.isEmpty());
+		System.out.println("@Test - testEmptyCollection");
+	}
+
+	@Test
+	public void testOneItemCollection() {
+		collection.add("itemA");
+		assertEquals(1, collection.size());
+		System.out.println("@Test - testOneItemCollection");
+	}
+}
