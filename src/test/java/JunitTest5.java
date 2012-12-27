9ae021b3ebfb0f0a124a21ffd778a3ab3df569ff
Alex <alex@Miracle.com>
2012-12-26 18:56:42 -0800

    Suite Test

---
+++ src/test/java/JunitTest5.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        JunitTest1.class,
+        JunitTest2.class
+})
+
+public class JunitTest5 {
+}
